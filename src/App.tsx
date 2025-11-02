6c5917d4650859c62a281cf2f2421e2ec6b202ec
marcos <marcos@exemplo.com>
2025-11-01 21:25:50 -0300

    coomit da Vi

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Header from './components/Cabecalho'
+import Cabecalho from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
-
-import './global.css'
+import EstiloGlobal from './styles'
 
 function App() {
   return (
     <>
-      <Header />
+      <EstiloGlobal />
+      <Cabecalho />
       <Hero />
       <div className="container">
         <ListaVagas />
--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,13 @@
-import styles from './Cabecalho.module.css'
+import styled from 'styled-components'
 
-const Cabecalho = () => (
-  <header className={styles.cabecalho}>
-    <h1>EBAC Jobs</h1>
-  </header>
-)
+const Cabecalho = styled.header`
+  background-color: '#a7727d';
+  color: #f9f5e7;
+  text-align: center;
+  padding: 24px 0;
+`
+function Titulo() {
+  return <Cabecalho>Ebac Jobs </Cabecalho>
+}
 
 export default Cabecalho
